543c016e270d756761f95de344e6769213632852
Kunal Singh <kunal.singh@canoo.com>
2015-12-17 10:39:59 +0530

    Added problem statement

---
+++ ProgrammingProblemsPrj/src/general/LeastNumberFinderWithSameDigit.java
@@ -0,0 +1,10 @@
+package general;
+
+/**
+ * Find next least number with same digit
+ * For ex. - input: 23654 -> output: 23456, input: 2654 -> output: 4256
+ *
+ */
+public class LeastNumberFinderWithSameDigit {
+
+}
